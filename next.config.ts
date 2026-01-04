a6215ca21a30416f4b3db5e850cd0ec8ca1f6814
Adexbam02 <nurainbamidelejnr@gmail.com>
2026-01-04 10:51:26 +0100

    Re-routing

--- next.config.ts
+++ next.config.ts
@@ -22,38 +22,38 @@ module.exports = withBundleAnalyzer({
 
 // I did this since we are doing a redirect from '/' to '/application' until we start the main website
 
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   async redirects() {
-//     return [
-//       {
-//         source: "/",
-//         destination: "/application",
-//         permanent: true,
-//       },
-//       {
-//         source: "/about",
-//         destination: "/application",
-//         permanent: true,
-//       },
-//       {
-//         source: "/projects",
-//         destination: "/application",
-//         permanent: true,
-//       },
-//       {
-//         source: "/ctrl-labs",
-//         destination: "/application",
-//         permanent: true,
-//       },
-//       {
-//         source: "/explore",
-//         destination: "/application",
-//         permanent: true,
-//       },
-//     ];
-//   },
-// };
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/application",
+        permanent: true,
+      },
+      {
+        source: "/about",
+        destination: "/application",
+        permanent: true,
+      },
+      {
+        source: "/projects",
+        destination: "/application",
+        permanent: true,
+      },
+      {
+        source: "/ctrl-labs",
+        destination: "/application",
+        permanent: true,
+      },
+      {
+        source: "/explore",
+        destination: "/application",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-// export default nextConfig;
+export default nextConfig;
